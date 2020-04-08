432c9f02e385da7cff72d56a93188af01ea0d5d1
Martin Brettschneider <martin.brettschneider@gmail.com>
2020-04-08 07:48:10 +0200

    Thumbor securityKey

--- src/Services/ThumborService.php
+++ src/Services/ThumborService.php
@@ -10,14 +10,34 @@ class ThumborService implements ServiceInterface
 	/** @var string */
 	protected $thumborUrl;
 
-	public function __construct(string $thumborUrl)
+	/** @var string */
+	protected $securityKey;
+
+	public function __construct(string $thumborUrl, ?string $securityKey = null)
 	{
 		$this->thumborUrl = $thumborUrl;
+		$this->securityKey = $securityKey;
 	}
 
 	public function processImage(string $imageUrl, ?string $modifier = null): string
 	{
-		return $this->thumborUrl . '/unsafe/' . ($modifier !== null ? $modifier . '/' : '') . Strings::replace($imageUrl, '~https?://~');
+		$path = ($modifier !== null ? $modifier . '/' : '') . Strings::replace($imageUrl, '~https?://~');
+
+		$signature = $this->securityKey !== null
+			? $this->sign($path)
+			: 'unsafe';
+
+		return $this->thumborUrl . '/' . $signature . '/' . $path;
+	}
+
+	protected function sign(string $path): string
+	{
+		$signature = hash_hmac('sha1', $path, $this->securityKey, true);
+
+		return strtr(
+			base64_encode($signature),
+			'/+', '_-'
+		);
 	}
 
 }
